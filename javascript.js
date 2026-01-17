c87a08fc77defcd332866f4e2445a69eee36497e
mwynott <mwynott1989@gmail.com>
2026-01-17 15:27:49 -0500

    Refactor conditional for playRound for simplicity and readability

--- javascript.js
+++ javascript.js
@@ -37,45 +37,40 @@
     let humanScore = 0;
     let computerScore = 0;
 
-//Create a function named playRound(humanChoice, computerChoice) 
-// <--uses human and computer choice as arguments
-   
+//Create a function named playRound
+// <--uses human and computer choices as arguments
+//playRound should console.log a string value for winner
+//Increment humanScore or computerScore variable based on winner
 
     function playRound(humanSelection, computerSelection) {
 
         humanSelection = humanSelection.toLowerCase();
         computerSelection = computerSelection.toLowerCase();
 
-        if (humanSelection === "rock" && computerSelection ==="paper") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Computer wins!`);
-        } else if (humanSelection === "paper" && computerSelection === "scissors") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Computer wins!`);
-        } else if (humanSelection === "scissors" && computerSelection === "rock") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Computer wins!`);
-        } else if (humanSelection === computerSelection) {
+        if (humanSelection === computerSelection) {
             console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
             It's a tie!`);
-        } else if (humanSelection === "rock" && computerSelection ==="scissors") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Human wins!`);
-        } else if (humanSelection === "paper" && computerSelection ==="rock") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Human wins!`);
-        } else if (humanSelection === "scissors" && computerSelection ==="paper") {
-            console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
-            Human wins!`);
-        }
-    }
-
+                } else if (
+                    (humanSelection === "rock" && computerSelection === "scissors") ||
+                    (humanSelection === "scissors" && computerSelection === "paper") ||
+                    (humanSelection === "paper" && computerSelection === "rock") 
+                )   {
+                    console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
+                    Human wins!`)
+                    humanScore += 1;
+                } else {
+                    console.log(`Human chose: ${humanSelection}! Computer chose: ${computerSelection}!
+                    Computer wins!`)
+                    computerScore += 1;
+                }
+            }
+        
     const humanSelection = getHumanChoice()
     const computerSelection = getComputerChoice()
 
     playRound(humanSelection, computerSelection);
 
-//playRound should console.log a string value for winner
+
 //Increment humanScore or computerScore variable based on winner
 
 
